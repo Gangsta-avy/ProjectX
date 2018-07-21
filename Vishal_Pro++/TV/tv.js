aacc4c33f954d789fc61bb0d93f91f1e03f7f009
vishal1406 <vishal14031998@gmail.com>
2018-07-21 10:01:06 +0530

    Guys edit everything in this File

---
+++ Vishal_Pro++/TV/tv.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+    $("#reset").click(function (e) {
+        location.reload();
+    });
+
+    $("#submit").click(function (e) {
+        var validate = Validate();
+        $("#message").html(validate);
+        if (validate.length == 0) {
+            CallAPI(1);
+        }
+    });
+
+    $("#message").on("click", ".result", function () {
+        var resourceId = $(this).attr("resourceId");
+        $.ajax({
+            url: "https://api.themoviedb.org/3/tv/" + resourceId + "?language=en-US",
+            data: {
+                api_key: "eb4cda00c52a1c0b7c868aec79418356"
+            },
+            dataType: 'json',
+            success: function (result, status, xhr) {
+                $("#modalTitleH4").html(result["name"]);
+
+                var image = result["poster_path"] == null ? "no-image.png" : "https://image.tmdb.org/t/p/w500/" + result["poster_path"];
+                var overview = result["overview"] == null ? "No information available" : result["overview"];
+
+                var resultHtml = "<p class=\"text-center\"><img src=\"" + image + "\"/></p><p>" + overview + "</p>";
+                resultHtml += "<p>Popularity: " + result["popularity"] + "</p><p>Rating: " + result["vote_average"] + "";
+
+                $("#modalBodyDiv").html(resultHtml)
+
+                $("#myModal").modal("show");
+            },
+            error: function (xhr, status, error) {
+                $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
+    });
+
+    $(document).ajaxStart(function () {
+        $(".imageDiv img").show();
+    });
+
+    $(document).ajaxStop(function () {
+        $(".imageDiv img").hide();
+    });
+
+    function CallAPI(page) {
+        $.ajax({
+            url: "https://api.themoviedb.org/3/search/tv?language=en-US&query=" + $("#searchInput").val() + "&page=" + page + "&include_adult=false",
+            data: { "api_key": "eb4cda00c52a1c0b7c868aec79418356" },
+            dataType: "json",
+            success: function (result, status, xhr) {
+                var resultHtml = $("<div class=\"resultDiv\"><p>Names</p>");
+                for (i = 0; i < result["results"].length; i++) {
+
+                    var image = result["results"][i]["poster_path"] == null ? "no-image.png" : "https://image.tmdb.org/t/p/w500/" + result["results"][i]["poster_path"];
+
+                    resultHtml.append("<div class=\"result\" resourceId=\"" + result["results"][i]["id"] + "\">" + "<img src=\"" + image + "\" />" + "<p><a>" + result["results"][i]["name"] + "</a></p></div>")
+                }
+
+                resultHtml.append("</div>");
+                $("#message").html(resultHtml);
+
+                Paging(result["total_pages"]);
+            },
+            error: function (xhr, status, error) {
+                $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
+    }
+
+    function Validate() {
+        var errorMessage = "";
+        if ($("#searchInput").val() == "") {
+            errorMessage += "► Enter Search Text";
+        }
+        return errorMessage;
+    }
+
+    function Paging(totalPage) {
+        var obj = $("#pagination").twbsPagination({
+            totalPages: totalPage,
+            visiblePages: 5,
+            onPageClick: function (event, page) {
+                CallAPI(page);
+            }
+        });
+    }
+
+});
--- tmdb/script.js
+++ tmdb/script.js
@@ -1,7 +1,7 @@
 const base_url="https://api.themoviedb.org/3/";
 const APIKEY = "f6642a9e75698dbd0dfd566716c561f2";
 const image_url="https://image.tmdb.org/t/p/w500";
-const test = "https://api.themoviedb.org/3/movie/550?api_key=f6642a9e75698dbd0dfd566716c561f2&callback=test";
+const test = "https://api.themoviedb.org/3/multi/550?api_key=f6642a9e75698dbd0dfd566716c561f2&callback=test";
 
 $('button').click(() => {
     const x = $('#search').val();
